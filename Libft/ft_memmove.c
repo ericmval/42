6b7d8141d098a6cf52319066cfaec090b6051106
Eric Martinez Val <emartin2@cbr9s1.42barcelona.com>
2023-09-20 23:20:01 +0200

    20 sep  42

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -10,4 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
 
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+    unsigned char *d;
+    const unsigned char *s;
+    size_t  i;
+    
+    d = (unsigned char *)dest;
+    s = (const unsigned char *) src;
+
+    if (d == s)
+        return (dest);
+    if (s < d && d < s + n)
+    {
+        i = n;
+        while ( i > 0)
+        {
+            d[i - 1] = s[i -1];
+            i--;
+        }
+    }
+    else
+    {
+        i = 0;
+        while (i < n)
+        {
+            d[i] = s[i];
+            i++;
+        }
+    }
+    return (dest);
+    }
+/*   
+int main (void)
+{
+    char inicio[5] = "casa";
+    char destino[7] = "pelota";
+    ft_memmove(destino,inicio, 5);
+    return (0);
+}
+*/

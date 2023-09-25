c183eeb2d103b52248a9df45070396f147e55160
eric linux <ericmval@gmail.com>
2023-09-25 23:13:00 +0200

    pequeño avance

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,42 +6,36 @@
 /*   By: emartin2 <emartin2@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 12:01:30 by emartin2          #+#    #+#             */
-/*   Updated: 2023/09/24 16:05:38 by emartin2         ###   ########.fr       */
+/*   Updated: 2023/09/25 16:50:27 by emartin2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <stdlib.h>
 #include "libft.h"
 #include <stdio.h>
 
-char	*ft_substr(char const *s, unsigned int start,size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t  count;
+	size_t	count;
 	char	*exit;
-	int 	i;
+	size_t	i;
 
+	if (!s)
+		return (NULL);
 	i = 0;
 	count = ft_strlen(s);
-	if (((size_t)start + len) >= count)
-		return ("");
-	s += start;
-	exit = (char *) malloc(len +1);
+	if (start >= count)
+		len = 0;
+	if (len > count)
+		len = count;
+	exit = (char *) malloc(len + 1);
 	if (!exit)
 		return (NULL);
-	while (len-- )
+	while (i < len && (start + i) < count)
 	{
-		exit[i] = s[i];
+		exit[i] = s[start + i];
 		i++;
 	}
 	exit[i] = '\0';
 	return (exit);
 }
-
-int main (void)
-{
-	char test[11] = "";
-	char *test2;
-	test2 = (ft_substr(test,5,0));
-	printf("%s\n",test2);
-	return (0);
-}
-

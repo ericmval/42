c4873e36f39c5fbb1ed2269411c5f119187828f3
Eric Martinez Val <emartin2@car9s1.42barcelona.com>
2023-10-07 15:03:03 +0200

    sabado 42

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,22 +6,73 @@
 /*   By: emartin2 <emartin2@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 21:50:39 by emartin2          #+#    #+#             */
-/*   Updated: 2023/10/05 22:12:14 by emartin2         ###   ########.fr       */
+/*   Updated: 2023/10/07 13:33:30 by emartin2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <stdio.h>
-#include "llibft/libft.h"
-#include "ft_printf.h"
+#include <stdarg.h>
 
-int ft_printf(char const *, ...)
+int ft_typesel (const char *text, int *i, va_list args,int *cont);
+static void ft_printer(char a, int *cont);
+
+int ft_printf(const char *text, ...)
 {
-	int	i;
 	va_list	args;
 	int	cont;
-	
-	return(0);
+	int i;
+
+	i = 0;
+	va_start(args, text);
+	cont = 0;
+	while (text[i])
+	{
+		if (text[i] == '%')
+		{
+			i++;
+			ft_typesel(text, &i, args, &cont);
+		}
+		else
+		{
+			ft_printer(text[i], &cont);
+			i++;
+		}
+	}
+	va_end(args);
+	return(cont);
 }
 
+static void ft_printer(char a,int *cont)
+{
+	write(1, &a, 1);
+	(*cont)++;
+}
+
+int ft_typesel (const char *text, int *i, va_list args,int *cont)
+{
+	if (text[*i] == '%')
+		ft_printer(text[*i], cont);
+	else if (text[*i] == 'c')
+		ft_printer((char)va_arg(args, int), cont);
+	else
+		return (-1);
+	(*i)++;
+	return (0);
+}
+
+int main (void)
+{
+	int original;
+	int ft;
+	char test1 = 'T';
+
+	ft = ft_printf("%c %%% bien \n", test1);
+	original = printf("%c %%% bien \n", test1);
+	printf("el original es:%d\n", original);
+	printf("el ft es:%d\n", ft);
+
+	return (0);
+}
 
 
